a8d9fa3dfd3496929258f73562e0fefc32965962
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-16 23:00:23 -0300

    Crea proyecto maven

---
+++ demo/src/main/java/com/vivero/Application.java
@@ -0,0 +1,8 @@
+package com.vivero;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ demo/src/test/java/com/vivero/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.vivero;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
