14d4f9088ce57cb78dfc6f0c4a2fcf0ff15431b5
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-17 13:03:36 -0300

    Agrega entidad Pedido

---
+++ demo/src/main/java/com/vivero/entidades/Pedido.java
@@ -0,0 +1,110 @@
+package com.vivero.entidades;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private int idPedido;
+
+    @Column(name = "codigo_pedido")
+    private int codigoPedido;
+
+    @Column(name = "comentario")
+    private String comentario;
+
+    @Column(name = "estado")
+    private String estado;
+
+    @Column(name = "fecha_entrega")
+    private LocalDate fechaEntrega; 
+
+    @Column(name = "fecha_esperada")
+    private LocalDate fechaEsperada; 
+
+    @Column(name = "fecha_pedido")
+    private LocalDate fechaPedido; 
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false) 
+    private Cliente cliente;
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getCodigoPedido() {
+        return codigoPedido;
+    }
+
+    public void setCodigoPedido(int codigoPedido) {
+        this.codigoPedido = codigoPedido;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public LocalDate getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(LocalDate fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public LocalDate getFechaEsperada() {
+        return fechaEsperada;
+    }
+
+    public void setFechaEsperada(LocalDate fechaEsperada) {
+        this.fechaEsperada = fechaEsperada;
+    }
+
+    public LocalDate getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public void setFechaPedido(LocalDate fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Pedido() {
+    }
+}
