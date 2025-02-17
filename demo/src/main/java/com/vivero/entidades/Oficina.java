0e5b0a3de1ab0125ac4c7b362b9035f69374951c
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-17 11:55:18 -0300

    Agrega entidad Oficina

--- demo/src/main/java/com/vivero/entidades/Oficina.java
+++ demo/src/main/java/com/vivero/entidades/Oficina.java
@@ -1,5 +1,97 @@
 package com.vivero.entidades;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "oficina")
 public class Oficina {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_oficina")
+    private int idOficina;
+
+    @Column(name = "ciudad", length = 50)
+    private String ciudad;
+
+    @Column(name = "codigo_postal", length = 10)
+    private String codigoPostal;
+
+    @Column(name = "pais", length = 50)
+    private String pais;
+
+    @Column(name = "region", length = 50)
+    private String region;
+
+    @Column(name = "telefono", length = 20)
+    private String telefono;
+
+    @OneToMany(mappedBy = "oficina")
+    private List<Empleado> empleado;
+
+    public Oficina() {
+    }
+
+    public int getIdOficina() {
+        return idOficina;
+    }
+
+    public void setIdOficina(int idOficina) {
+        this.idOficina = idOficina;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Empleado> getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(List<Empleado> empleado) {
+        this.empleado = empleado;
+    }
 }
