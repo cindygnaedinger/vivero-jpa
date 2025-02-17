6a1bf0738a319e88437166204ca5cfed85a3a9dc
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-17 12:50:58 -0300

    Agrega entidad DetallePedido

---
+++ demo/src/main/java/com/vivero/entidades/DetallePedido.java
@@ -0,0 +1,88 @@
+package com.vivero.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "detalle_pedido")
+public class DetallePedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_detalle_pedido")
+    private int idDetallePedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", nullable = false) 
+    private Producto producto;
+
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    @Column(name = "numero_linea")
+    private int numeroLinea;
+
+    @Column(name = "precio_unidad")
+    private double precioUnidad;
+
+    public DetallePedido() {
+    }
+
+    public int getIdDetallePedido() {
+        return idDetallePedido;
+    }
+
+    public void setIdDetallePedido(int idDetallePedido) {
+        this.idDetallePedido = idDetallePedido;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getNumeroLinea() {
+        return numeroLinea;
+    }
+
+    public void setNumeroLinea(int numeroLinea) {
+        this.numeroLinea = numeroLinea;
+    }
+
+    public double getPrecioUnidad() {
+        return precioUnidad;
+    }
+
+    public void setPrecioUnidad(double precioUnidad) {
+        this.precioUnidad = precioUnidad;
+    }
+    
+}
